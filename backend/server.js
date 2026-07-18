16b5b987fb840db90b44a448d6e2af7e73fd941b
Subhajit Ghatak <subhajitghatak1921@gmail.com>
2026-07-18 16:47:54 +0530

    configuration for vercel deploy

--- backend/server.js
+++ backend/server.js
@@ -64,4 +64,4 @@ const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => console.log("Server running"));
 }
-module.exports = app;
+export default app;
